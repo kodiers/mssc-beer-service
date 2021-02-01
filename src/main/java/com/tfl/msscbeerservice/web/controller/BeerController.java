772b9c2349ea98c86615d1508937a47e26879383
Victor Yamchinov <kodiers@gmail.com>
2021-02-01 20:27:20 +0300

    added BeerController

---
+++ src/main/java/com/tfl/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,28 @@
+package com.tfl.msscbeerservice.web.controller;
+
+import com.tfl.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable UUID beerId) {
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable UUID beerId, @RequestBody BeerDto beerDto) {
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
