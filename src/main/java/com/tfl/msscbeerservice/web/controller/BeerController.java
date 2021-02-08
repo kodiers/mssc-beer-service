f2732651b78036d94de9e580e9bc0d98f793a381
Victor Yamchinov <kodiers@gmail.com>
2021-02-08 20:48:39 +0300

    added ControllerAdvice

--- src/main/java/com/tfl/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/tfl/msscbeerservice/web/controller/BeerController.java
@@ -3,6 +3,7 @@ package com.tfl.msscbeerservice.web.controller;
 import com.tfl.msscbeerservice.web.model.BeerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -17,12 +18,12 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+    public ResponseEntity saveNewBeer(@RequestBody @Validated BeerDto beerDto) {
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable UUID beerId, @RequestBody BeerDto beerDto) {
+    public ResponseEntity updateBeerById(@PathVariable UUID beerId, @RequestBody @Validated BeerDto beerDto) {
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
---
+++ src/main/java/com/tfl/msscbeerservice/web/controller/MvcExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.tfl.msscbeerservice.web.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class MvcExceptionHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List<String>> validationExceptionHandler(ConstraintViolationException exception) {
+        List<String> errorList = new ArrayList<>(exception.getConstraintViolations().size());
+        exception.getConstraintViolations().forEach(error -> errorList.add(error.toString()));
+        return new ResponseEntity<>(errorList, HttpStatus.BAD_REQUEST);
+    }
+}
